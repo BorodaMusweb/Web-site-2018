29c006e7679de8ccb771dc2039b0788cdd64c13e
Dermolenko <boroda@musweb.ru>
2018-03-07 21:23:19 +0300

    add file js | add file css | added changes index_1.html

---
+++ index_1.js
@@ -0,0 +1,29 @@
+<script>
+
+var slider = {
+  slides:['6.jpg', '9.jpg', '20.jpg'],
+  frame:0,
+  set: function(image){
+    document.getElementByid("src").style.backgroundImage = "url("+image+")";
+  },
+  init: function(){
+    this.set(this.slides[this.frame]);
+  },
+  left: function(){
+    this.frame--;
+    if(this.frame < 0) this.frame = this.slides.length-1;
+    this.set(this.slides[this.frame]);
+  },
+  right: function(){
+    this.frame++;
+    if(this.frame == this.slides.length) this.frame = 0;
+    this.set(this.slides[this.frame]);
+  }
+};
+window.onload= function(){
+  slider.init();
+  setInterval(function(){
+    slider.right();
+},5000);
+}
+</script>
