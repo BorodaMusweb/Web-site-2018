1f6ad81d30003f5af7b6af468041114bf93fb35b
Dermolenko <boroda@musweb.ru>
2018-04-09 22:50:49 +0300

    add new kod js/html/css

--- index_1.js
+++ index_1.js
@@ -1,29 +1,48 @@
-<script>
-
-var slider = {
-  slides:['6.jpg', '9.jpg', '20.jpg'],
-  frame:0,
-  set: function(image){
-    document.getElementByid("src").style.backgroundImage = "url("+image+")";
-  },
-  init: function(){
-    this.set(this.slides[this.frame]);
-  },
-  left: function(){
-    this.frame--;
-    if(this.frame < 0) this.frame = this.slides.length-1;
-    this.set(this.slides[this.frame]);
-  },
-  right: function(){
-    this.frame++;
-    if(this.frame == this.slides.length) this.frame = 0;
-    this.set(this.slides[this.frame]);
-  }
-};
-window.onload= function(){
-  slider.init();
-  setInterval(function(){
-    slider.right();
-},5000);
+var slideIndex = 1;
+showSlides(slideIndex);
+// Next/previous controls
+function plusSlides(x) {
+  showSlides(slideIndex += x);
 }
-</script>
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {
+    slideIndex = 1
+  }
+  if (n < 1) {
+    slideIndex = slides.length
+  }
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
+
+
+// var slideIndex = 0;
+// showSlides();
+//
+// function showSlides() {
+//     var i;
+//     var slides = document.getElementsByClassName("mySlides");
+//     for (i = 0; i < slides.length; i++) {
+//         slides[i].style.display = "none";
+//     }
+//     slideIndex++;
+//     if (slideIndex > slides.length) {slideIndex = 1}
+//     slides[slideIndex-1].style.display = "block";
+//     setTimeout(showSlides, 2000); // Change image every 2 seconds
+// }
